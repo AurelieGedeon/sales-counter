bd2f7b92c0702c2b4edc050026dd21a3f44219cb
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-29 13:58:50 -0500

    moved components around to make app cleaner

--- src/App.js
+++ src/App.js
@@ -1,52 +1,16 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import "./App.css";
+import Counter from "./components/Counter/Counter";
+import Modal from "./components/Modal/Modal";
 
 function App() {
-  const [counter, setCounter] = useState(0)
-
-  const reoccurringButtons = [
-    {
-      name: 'mascara',
-      price: 7.99,
-      inventory: 200
-    },
-    {
-      name: 'lipstick',
-      price: 5.99,
-      inventory: 150
-    },
-    {
-      name: 'eyeliner',
-      price: 6.99,
-      inventory: 100
-    },
-    {
-      name:'blush',
-      price: 12.50,
-      inventory: 75
-  }
-  ]
-  let totalInventory = reoccurringButtons.reduce((accumulator, button) => {
-    return accumulator + button.inventory
-  }, 0)
-  
-  const handleButtonClick = (price) => {
-    setCounter(counter + price)
-  }
-
   return (
     <div className="App">
-     <header className="App-header">
-      <h1> Counter: {counter.toFixed(2)} </h1>
-      <div className='button-container'>
-          <button onClick={() => setCounter(0)}> RESET </button>
-          {reoccurringButtons.map((button) => (
-            <button onClick={() => handleButtonClick(button.price)}>{button.name}</button>
-          ))}
-      </div>
-     </header> 
+      <header className="App-header">
+        <Counter />
+        <Modal />
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,19 +1,28 @@
-import ".../App.css"
-import "./Modal.css"
-import { useState } from "react"
+import ".../App.css";
+import "./Modal.css";
+import { useState } from "react";
 
 export default function Modal() {
-    const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false);
 
-    const toggleModal = () => {
-        setModal(!modal)
-    }
+  const toggleModal = () => {
+    setModal(!modal);
+  };
 
-    return(
-        <>
-        <button 
-        onClick={toggleModal}
-        className="btn-modal">Add Button</button>
-        </>
-    )
+  return (
+    <>
+      <button onClick={toggleModal} className="btn-modal">
+        Add Button
+      </button>
+      <div className="modal">
+        <div className="overlay"></div>
+        <div className="modal-content">
+          <h2>Hello Modal!</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam maiores optio, vel modi voluptates magni, mollitia architecto nulla debitis tempore, accusantium in tempora? Tenetur expedita ea ab consequuntur praesentium? Ut amet fugit animi illum doloribus magni vel facilis laboriosam illo delectus. Fugit deleniti mollitia itaque voluptatum nulla fugiat illo at.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
