814cf4b0c2e0d0258e775a9d46cdfdc478675faf
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-29 14:09:22 -0500

    made counter its own component folder; added css for modal

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  const reoccurringButtons = [
+    {
+      name: "mascara",
+      price: 7.99,
+      inventory: 200,
+    },
+    {
+      name: "lipstick",
+      price: 5.99,
+      inventory: 150,
+    },
+    {
+      name: "eyeliner",
+      price: 6.99,
+      inventory: 100,
+    },
+    {
+      name: "blush",
+      price: 12.5,
+      inventory: 75,
+    },
+  ];
+  let totalInventory = reoccurringButtons.reduce((accumulator, button) => {
+    return accumulator + button.inventory;
+  }, 0);
+  const handleButtonClick = (price) => {
+    setCounter(counter + price);
+  };
+
+  return (
+    <>
+      <h1> Counter: {counter.toFixed(2)} </h1>
+      <div className="button-container">
+        <button onClick={() => setCounter(0)}> RESET </button>
+        {reoccurringButtons.map((button) => (
+          <button onClick={() => handleButtonClick(button.price)}>
+            {button.name}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import ".../App.css";
+import "./Modal.css";
 import "./Modal.css";
 import { useState } from "react";
 
@@ -14,15 +14,19 @@ export default function Modal() {
       <button onClick={toggleModal} className="btn-modal">
         Add Button
       </button>
-      <div className="modal">
-        <div className="overlay"></div>
-        <div className="modal-content">
-          <h2>Hello Modal!</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam maiores optio, vel modi voluptates magni, mollitia architecto nulla debitis tempore, accusantium in tempora? Tenetur expedita ea ab consequuntur praesentium? Ut amet fugit animi illum doloribus magni vel facilis laboriosam illo delectus. Fugit deleniti mollitia itaque voluptatum nulla fugiat illo at.
-          </p>
+      {modal && (
+        <div className="modal">
+            <div className="overlay" onClick={toggleModal}></div>
+            <div className="modal-content">
+            <h2>Hello Modal!</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam maiores optio, vel modi voluptates magni, mollitia architecto nulla debitis tempore, accusantium in tempora? Tenetur expedita ea ab consequuntur praesentium? Ut amet fugit animi illum doloribus magni vel facilis laboriosam illo delectus. Fugit deleniti mollitia itaque voluptatum nulla fugiat illo at.
+            </p>
+            <button className="close-modal" onClick={toggleModal}>X</button>
+            </div>
         </div>
-      </div>
+
+      )}
     </>
   );
 }
