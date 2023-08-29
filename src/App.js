6848ce8fbc9211d7ce51453adf39524b627b8971
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-29 13:16:42 -0500

    Changed initial hardoced buttons to map based on objects within array

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0)
-  let a = {
-    name: 'mascara',
-    price: 7.99,
-    inventory: 200
+
+  const reoccurringButtons = [
+    {
+      name: 'mascara',
+      price: 7.99,
+      inventory: 200
+    },
+    {
+      name: 'lipstick',
+      price: 5.99,
+      inventory: 150
+    },
+    {
+      name: 'eyeliner',
+      price: 6.99,
+      inventory: 100
+    }
+  ]
+  let totalInventory = reoccurringButtons.reduce((accumulator, button) => {
+    return accumulator + button.inventory
+  }, 0)
+  
+  const handleButtonClick = (price) => {
+    setCounter(counter + price)
   }
-  let b = {
-    name: 'lipstick',
-    price: 5.99,
-    inventory: 150
-  }
-  let c = {
-    name: 'eyeliner',
-    price: 6.99,
-    inventory: 100
-  }
-  let totalInventory = a.inventory + b.inventory + c.inventory
-  console.log(totalInventory)
-  // useEffect(() => {
-  //   if (counter > 100) { 
 
   return (
     <div className="App">
@@ -29,9 +35,9 @@ function App() {
       <h1> Counter: {counter.toFixed(2)} </h1>
       <div className='button-container'>
           <button onClick={() => setCounter(0)}> RESET </button>
-          <button onClick={() => setCounter(counter + a.price)}> {a.name}</button>
-          <button onClick={() => setCounter(counter + b.price)}> {b.name}</button>
-          <button onClick={() => setCounter(counter + c.price)}> {c.name}</button>
+          {reoccurringButtons.map((button) => (
+            <button onClick={() => handleButtonClick(button.price)}>{button.name}</button>
+          ))}
       </div>
      </header> 
     </div>
